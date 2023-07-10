28f7854fc4d4951309c50e6dd1c18aa744fb31d7
omarabdel00 <omarelmetwaly03@yahoo.com>
2023-07-10 14:11:03 -0700

    add polymorphism assignment

---
+++ Polymorphism Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Step 1: Create the IQuittable interface
+interface IQuittable
+{
+    void Quit();
+}
+
+// Step 2: Inherit the IQuittable interface in the Employee class
+class Employee : IQuittable
+{
+    public string Name { get; set; }
+
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+    // Implement the Quit method defined in the IQuittable interface
+    public void Quit()
+    {
+        Console.WriteLine($"Employee {Name} is quitting...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Step 3: Use polymorphism to create an object of type IQuittable
+        // and call the Quit() method on it
+        IQuittable quittableEmployee = new Employee("John Doe");
+        quittableEmployee.Quit();
+    }
+}
