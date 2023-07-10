40135a1e980d87d6aeaf6c173455c9f8cdaf9e94
omarabdel00 <omarelmetwaly03@yahoo.com>
2023-07-10 14:36:40 -0700

    add Final Assignment Submission

---
+++ Final Assignment Submission/CodeFirstDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+namespace EF6CodeFirstDemo
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext()
+        {
+
+        }
+        // Entities        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Demo completed.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Final Assignment Submission/CodeFirstDemo/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF6CodeFirstDemo
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        //fully defined relationship
+        public int? GradeId { get; set; }
+
+        public virtual Grade Grade { get; set; }
+    }
+}
