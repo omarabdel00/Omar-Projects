fee46807da25c2b4273ca65f95855b6721fb56fe
omarabdel00 <omarelmetwaly03@yahoo.com>
2023-07-28 13:44:56 -0700

    updated the final assignment

--- EF6-Code-First-Demo-master/README.md
+++
@@ -1,2 +0,0 @@
-# EF6-Code-First-Demo
-Entity Framework 6 Code-First Demo Project
---
+++ Final-Assignment-Submission/codefirstdemo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace codefirstdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
